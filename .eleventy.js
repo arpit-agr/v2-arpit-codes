b46270f9e3be567993463ff3f7f2a71ea5f7deaa
arpit-agr <arpit.agrawal2502@gmail.com>
2022-06-10 11:00:45 +0530

    install and configure html minifier

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,25 @@
 const directoryOutputPlugin = require("@11ty/eleventy-plugin-directory-output");
+const htmlmin = require("html-minifier");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.setQuietMode(true);
   eleventyConfig.addPlugin(directoryOutputPlugin);
+
+  //Transforms
+  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+    // Eleventy 1.0+: use this.inputPath and this.outputPath instead
+    if( this.outputPath && this.outputPath.endsWith(".html") ) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+        collapseBooleanAttributes: true
+      });
+      return minified;
+    }
+
+    return content;
+  });
 
   return {
     // dataTemplateEngine: "njk",
