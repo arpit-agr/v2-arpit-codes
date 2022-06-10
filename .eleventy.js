e59023cdb81a2900f0bd108d4540948f6d0af0e1
arpit-agr <arpit.agrawal2502@gmail.com>
2022-06-10 10:47:26 +0530

    configure template engine for md and html

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
 module.exports = function(eleventyConfig) {
   return {
+    // dataTemplateEngine: "njk",
+		markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
 		dir: {
 			input: 'src',
 			output: 'public'
