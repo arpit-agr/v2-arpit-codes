14ec73564a4aa2ede31a927da700dd59e47ba517
arpit-agr <arpit.agrawal2502@gmail.com>
2024-01-06 10:44:57 +0530

    Adjust the character limit in the excerpt function

--- config/filters/excerpt.js
+++ config/filters/excerpt.js
@@ -1,6 +1,6 @@
 const excerpt = (post) => {
 	const content = post.replace(/(<([^>]+)>)/gi, "");
-	return content.substr(0, content.lastIndexOf(" ", 160)) + " […]";
+	return content.substr(0, content.lastIndexOf(" ", 300)) + " […]";
 };
 
 module.exports = excerpt;
