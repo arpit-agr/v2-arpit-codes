81db3b6c1fc85d36413e59087b82237e28843a8f
arpit-agr <arpit.agrawal2502@gmail.com>
2024-01-10 14:41:00 +0530

    Update excerpt function to limit output to 150 characters

--- config/filters/excerpt.js
+++ config/filters/excerpt.js
@@ -1,6 +1,6 @@
 const excerpt = (post) => {
 	const content = post.replace(/(<([^>]+)>)/gi, "");
-	return content.substr(0, content.lastIndexOf(" ", 300)) + " […]";
+	return content.substr(0, content.lastIndexOf(" ", 150)) + " […]";
 };
 
 module.exports = excerpt;
